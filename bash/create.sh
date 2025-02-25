ed837ec287d1cfd2d5627e43b6c7f442427ef1d4
Guilherme Lenfers <guilherme.lenfers@alicredito.com.br>
2025-02-25 07:59:02 -0300

    Added VPC ID fetching from output command

--- bash/create.sh
+++ bash/create.sh
@@ -3,12 +3,16 @@
 # TODO tag name variable
 
 echo "Creating VPC resources..."
-aws ec2 create-vpc \
-    --cidr-block 10.1.0.0/24 \
-    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=aws-101-cli-vpc}]'
+
+vpc_id="$(aws ec2 create-vpc \
+    --cidr-block '10.1.0.0/24' \
+    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=aws-101-cli-vpc}]' \
+    --output json |
+    jq -r '.Vpc.VpcId')"
+echo "VPC Id: $vpc_id"
 
 # TODO Create subnets
 # TODO Create route tables
 # TODO Create NAT Gateways
 
-echo "Creating VPC resources... DONE!"
+echo "Successfully created VPC resources!"
