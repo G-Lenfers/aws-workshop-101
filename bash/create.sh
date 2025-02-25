a6ff0cb5382bc6d47b0c507f5a98cd4ccbe3f22e
Guilherme Lenfers <guilherme.lenfers@alicredito.com.br>
2025-02-25 08:08:26 -0300

    Moved tag name to a proper constant

--- bash/create.sh
+++ bash/create.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# TODO tag name variable
+TAG_NAME="aws-101-cli-vpc"
 
 echo "Creating VPC resources..."
 
 vpc_id="$(aws ec2 create-vpc \
     --cidr-block '10.1.0.0/24' \
-    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=aws-101-cli-vpc}]' \
+    --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=$TAG_NAME}]" \
     --output json |
     jq -r '.Vpc.VpcId')"
 echo "VPC Id: $vpc_id"
