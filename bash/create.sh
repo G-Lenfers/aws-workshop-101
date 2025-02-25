e387ddb9c4c1299804cf05628a25dd5b154e400d
Guilherme Lenfers <guilherme.lenfers@alicredito.com.br>
2025-02-25 07:20:23 -0300

    Added some print statements and TODO reminders

--- bash/create.sh
+++ bash/create.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# TODO tag name variable
+
+echo "Creating VPC resources..."
 aws ec2 create-vpc \
     --cidr-block 10.1.0.0/24 \
     --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=aws-101-cli-vpc}]'
+
+# TODO Create subnets
+# TODO Create route tables
+# TODO Create NAT Gateways
+
+echo "Creating VPC resources... DONE!"
