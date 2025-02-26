b7830042b691ae30be88347cdca63126cb4cda5d
Guilherme Lenfers <guilherme.lenfers@alicredito.com.br>
2025-02-26 07:34:53 -0300

    Created VPC subnet

--- bash/create.sh
+++ bash/create.sh
@@ -11,6 +11,12 @@ vpc_id="$(aws ec2 create-vpc \
     jq -r '.Vpc.VpcId')"
 echo "VPC Id: $vpc_id"
 
+aws ec2 create-subnet \
+    --vpc-id "$vpc_id" \
+    --cidr-block '10.1.0.0/28' \
+    --availability-zone 'us-east-1a' \
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$TAG_NAME}]"
+
 # TODO Create subnets
 # TODO Create route tables
 # TODO Create NAT Gateways
