fb53fa54a12edb617ff942970d26cdd6c3275db5
Guilherme Lenfers <guilherme.lenfers@alicredito.com.br>
2025-02-26 07:59:33 -0300

    Improved subnet creation handling

--- bash/create.sh
+++ bash/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG_NAME="aws-101-cli-vpc"
+TAG_NAME="aws-101-cli-vpc"  # TODO 'vpc' shouldn't be here
 
 echo "Creating VPC resources..."
 
@@ -11,11 +11,14 @@ vpc_id="$(aws ec2 create-vpc \
     jq -r '.Vpc.VpcId')"
 echo "VPC Id: $vpc_id"
 
-aws ec2 create-subnet \
+subnet_1a_id="$(aws ec2 create-subnet \
     --vpc-id "$vpc_id" \
     --cidr-block '10.1.0.0/28' \
     --availability-zone 'us-east-1a' \
-    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$TAG_NAME}]"
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$TAG_NAME}]" \
+    --output json |
+    jq -r '.Subnet.SubnetId')"  &&
+echo "Subnet 1a ID: $subnet_1a_id"
 
 # TODO Create subnets
 # TODO Create route tables
