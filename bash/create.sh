d36f933f77e1d6c77484050ad68968fa40a19196
Guilherme Lenfers <guilherme.lenfers@alicredito.com.br>
2025-02-26 08:12:21 -0300

    Improved tag name handling

--- bash/create.sh
+++ bash/create.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-TAG_NAME="aws-101-cli-vpc"  # TODO 'vpc' shouldn't be here
+TAG_NAME="aws-101-cli"
 
 echo "Creating VPC resources..."
 
 vpc_id="$(aws ec2 create-vpc \
     --cidr-block '10.1.0.0/24' \
-    --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=$TAG_NAME}]" \
+    --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=$TAG_NAME-vpc}]" \
     --output json |
     jq -r '.Vpc.VpcId')"
 echo "VPC Id: $vpc_id"
 
-subnet_1a_id="$(aws ec2 create-subnet \
+subnet_1a_public1_id="$(aws ec2 create-subnet \
     --vpc-id "$vpc_id" \
     --cidr-block '10.1.0.0/28' \
     --availability-zone 'us-east-1a' \
-    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$TAG_NAME}]" \
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$TAG_NAME-subnet-public1}]" \
     --output json |
     jq -r '.Subnet.SubnetId')"  &&
-echo "Subnet 1a ID: $subnet_1a_id"
+echo "Subnet 1a public1 ID: $subnet_1a_public1_id"
 
 # TODO Create subnets
 # TODO Create route tables
--- bash/destroy.sh
+++ bash/destroy.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-TAG_NAME="aws-101-cli-vpc"  # TODO 'vpc' shouldn't be here
+TAG_NAME="aws-101-cli"
 
 echo "Destroying VPC resources..."
 
 vpc_id="$(aws ec2 describe-vpcs \
-    --filters Name=tag:Name,Values=$TAG_NAME |
+    --filters Name=tag:Name,Values="$TAG_NAME-vpc" |
     jq -r '.Vpcs[0].VpcId')"
 
-subnet_1a_id="$(aws ec2 describe-subnets \
-    --filters Name=vpc-id,Values="$vpc_id" Name=tag:Name,Values=$TAG_NAME Name=availability-zone,Values=us-east-1a |
+subnet_1a_public1_id="$(aws ec2 describe-subnets \
+    --filters \
+        Name=vpc-id,Values="$vpc_id" \
+        Name=tag:Name,Values="$TAG_NAME-subnet-public1" \
+        Name=availability-zone,Values=us-east-1a |
     jq -r '.Subnets[0].SubnetId')"
-
 aws ec2 delete-subnet \
-    --subnet-id "$subnet_1a_id"
-echo "Deleted Subnet: $subnet_1a_id!"
+    --subnet-id "$subnet_1a_public1_id"
+echo "Deleted Subnet: $subnet_1a_public1_id!"
 
 aws ec2 delete-vpc \
     --vpc-id "$vpc_id"
-
 echo "Deleted VPC: $vpc_id!"
