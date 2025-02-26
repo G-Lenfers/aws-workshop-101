52efb49a827c6fa8c05b6854ea0f6bae12f60d00
Guilherme Lenfers <guilherme.lenfers@alicredito.com.br>
2025-02-26 07:59:51 -0300

    Added subnet deletion

--- bash/destroy.sh
+++ bash/destroy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-TAG_NAME="aws-101-cli-vpc"
+TAG_NAME="aws-101-cli-vpc"  # TODO 'vpc' shouldn't be here
 
 echo "Destroying VPC resources..."
 
 vpc_id="$(aws ec2 describe-vpcs \
     --filters Name=tag:Name,Values=$TAG_NAME |
     jq -r '.Vpcs[0].VpcId')"
+
+subnet_1a_id="$(aws ec2 describe-subnets \
+    --filters Name=vpc-id,Values="$vpc_id" Name=tag:Name,Values=$TAG_NAME Name=availability-zone,Values=us-east-1a |
+    jq -r '.Subnets[0].SubnetId')"
+
+aws ec2 delete-subnet \
+    --subnet-id "$subnet_1a_id"
 
 aws ec2 delete-vpc \
     --vpc-id "$vpc_id"
