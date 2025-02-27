33fd2bd114012bbaa0f3cb558a029a0cb6780c68
Guilherme Lenfers <guilherme.lenfers@alicredito.com.br>
2025-02-27 07:55:49 -0300

    Refactored implementation of deleting subnets to a dedicated function

--- bash/destroy.sh
+++ bash/destroy.sh
@@ -2,21 +2,26 @@
 
 TAG_NAME="aws-101-cli"
 
+function destroy_subnet () {
+    tag_name_complement=$1
+    subnet_id="$(aws ec2 describe-subnets \
+        --filters \
+            Name=vpc-id,Values="$vpc_id" \
+            Name=tag:Name,Values="$TAG_NAME-subnet-$tag_name_complement" \
+            Name=availability-zone,Values=us-east-1a |
+        jq -r '.Subnets[0].SubnetId')"
+    aws ec2 delete-subnet \
+        --subnet-id "$subnet_id"
+    echo "Deleted Subnet: $subnet_id"
+}
+
 echo "Destroying VPC resources..."
 
 vpc_id="$(aws ec2 describe-vpcs \
     --filters Name=tag:Name,Values="$TAG_NAME-vpc" |
     jq -r '.Vpcs[0].VpcId')"
 
-subnet_1a_public1_id="$(aws ec2 describe-subnets \
-    --filters \
-        Name=vpc-id,Values="$vpc_id" \
-        Name=tag:Name,Values="$TAG_NAME-subnet-public1" \
-        Name=availability-zone,Values=us-east-1a |
-    jq -r '.Subnets[0].SubnetId')"
-aws ec2 delete-subnet \
-    --subnet-id "$subnet_1a_public1_id"
-echo "Deleted Subnet: $subnet_1a_public1_id!"
+destroy_subnet "public1"
 
 aws ec2 delete-vpc \
     --vpc-id "$vpc_id"
