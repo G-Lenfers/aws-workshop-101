d7f668818b5abf7f2d55c40aa7822f7eaf7d425c
Guilherme Lenfers <guilherme.lenfers@alicredito.com.br>
2025-02-27 08:05:08 -0300

    Fixed CIDR block conflicts

--- bash/create.sh
+++ bash/create.sh
@@ -4,13 +4,14 @@ TAG_NAME="aws-101-cli"
 
 function create_subnet () {
     tag_name_complement=$1
+    cidr_block=$2
     aws ec2 create-subnet \
-    --vpc-id "$vpc_id" \
-    --cidr-block '10.1.0.0/28' \
-    --availability-zone 'us-east-1a' \
-    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$TAG_NAME-subnet-$tag_name_complement}]" \
-    --output json |
-    jq -r '.Subnet.SubnetId'
+        --vpc-id "$vpc_id" \
+        --cidr-block "$cidr_block" \
+        --availability-zone 'us-east-1a' \
+        --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$TAG_NAME-subnet-$tag_name_complement}]" \
+        --output json |
+        jq -r '.Subnet.SubnetId'
 }
 
 echo "Creating VPC resources..."
@@ -32,10 +33,10 @@ aws ec2 modify-vpc-attribute \
 
 # TODO Create S3 Endpoint
 
-subnet_1a_public1_id="$(create_subnet 'public1')" && echo "Subnet 1a public1 ID: $subnet_1a_public1_id"
-subnet_1a_private1_id="$(create_subnet 'private1')" && echo "Subnet 1a private1 ID: $subnet_1a_private1_id"
-subnet_1b_public2_id="$(create_subnet 'public2')" && echo "Subnet 1b public2 ID: $subnet_1b_public2_id"
-subnet_1b_private2_id="$(create_subnet 'private2')" && echo "Subnet 1b private2 ID: $subnet_1b_private2_id"
+subnet_1a_public1_id="$(create_subnet 'public1' '10.1.0.0/28')" && echo "Subnet 1a public1 ID: $subnet_1a_public1_id"
+subnet_1a_private1_id="$(create_subnet 'private1' '10.1.0.16/28')" && echo "Subnet 1a private1 ID: $subnet_1a_private1_id"
+subnet_1b_public2_id="$(create_subnet 'public2' '10.1.0.128/28')" && echo "Subnet 1b public2 ID: $subnet_1b_public2_id"
+subnet_1b_private2_id="$(create_subnet 'private2' '10.1.0.144/28')" && echo "Subnet 1b private2 ID: $subnet_1b_private2_id"
 
 # TODO Create Internet Gateways
 # TODO attach IGs to the public subnets
