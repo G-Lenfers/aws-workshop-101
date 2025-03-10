300acc04790b3f0a900d4a12a5825edd51aef8a7
Guilherme Lenfers <guilherme.lenfers@alicredito.com.br>
2025-03-10 08:08:40 -0300

    Fixed and improved function parameters usage

--- bash/create.sh
+++ bash/create.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TAG_NAME="aws-101-cli"
+REGION="us-east-1"
 
 function create_vpc () {
     _cidr_block=$1
@@ -35,13 +36,14 @@ function create_vpc_endpoint () {
 }
 
 function create_subnet () {
-    _tag_name_complement=$1
-    _vpc_id=$2
-    _cidr_block=$3
+    _vpc_id=$1
+    _cidr_block=$2
+    _availability_zone=$3
+    _tag_name_complement=$4
     aws ec2 create-subnet \
         --vpc-id "$_vpc_id" \
         --cidr-block "$_cidr_block" \
-        --availability-zone 'us-east-1a' \
+        --availability-zone "$REGION$_availability_zone" \
         --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$TAG_NAME-subnet-$_tag_name_complement}]" \
         --output json |
         jq -r '.Subnet.SubnetId'
@@ -57,10 +59,10 @@ enable_vpc_dns
 vpce_s3_id=$(create_vpc_endpoint "$vpc_id" Gateway com.amazonaws.us-east-1.s3 s3)
 echo "VPC S3 endpoint ID: $vpce_s3_id"
 
-subnet_1a_public1_id="$(create_subnet 'public1' '10.1.0.0/28')" && echo "Subnet 1a public1 ID: $subnet_1a_public1_id"
-subnet_1a_private1_id="$(create_subnet 'private1' '10.1.0.16/28')" && echo "Subnet 1a private1 ID: $subnet_1a_private1_id"
-subnet_1b_public2_id="$(create_subnet 'public2' '10.1.0.128/28')" && echo "Subnet 1b public2 ID: $subnet_1b_public2_id"
-subnet_1b_private2_id="$(create_subnet 'private2' '10.1.0.144/28')" && echo "Subnet 1b private2 ID: $subnet_1b_private2_id"
+subnet_1a_public1_id="$(create_subnet "$vpc_id" '10.1.0.0/28' 'a' 'public1')" && echo "Subnet 1a public1 ID: $subnet_1a_public1_id"
+subnet_1a_private1_id="$(create_subnet "$vpc_id" '10.1.0.16/28' 'a' 'private1')" && echo "Subnet 1a private1 ID: $subnet_1a_private1_id"
+subnet_1b_public2_id="$(create_subnet "$vpc_id" '10.1.0.128/28' 'b' 'public2')" && echo "Subnet 1b public2 ID: $subnet_1b_public2_id"
+subnet_1b_private2_id="$(create_subnet "$vpc_id" '10.1.0.144/28' 'b' 'private2')" && echo "Subnet 1b private2 ID: $subnet_1b_private2_id"
 
 # TODO Create Internet Gateways
 # TODO attach IGs to the public subnets
